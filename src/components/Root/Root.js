c6fede5d15a6d82dc7e571d99ca318db335394b4
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-13 22:52:09 +0100

    create main structure

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -13,16 +13,34 @@ const Root = () => {
           <FontAwesomeIcon icon={faFolderOpen} className={styles.header__option} />
           <FontAwesomeIcon icon={faSyncAlt} className={styles.header__option} />
         </div>
-        <div className={styles.header__round}></div>
+        <p className={`${styles.header__round} ${styles.noSelect}`}>Round 2</p>
       </header>
-      <main className={styles.main}></main>
-      <footer className={styles.footer}>
-        <div className={styles.footer__stats}>
-          <div className={styles.stats__ranking}></div>
-          <div className={styles.stats__history}></div>
+      <main className={styles.main}>
+        <div className={`${styles.main__backgroundTextWrapper} ${styles.noSelect}`}>
+          <h1 className={styles.main__backgroundTextWrapper_text}>Blackjack pays 3 to 2</h1>
+          <h2 className={styles.main__backgroundTextWrapper_text}>Dealer must stand on 17 and must draw to 16</h2>
         </div>
-        <div className={styles.footer__player}></div>
-        <div className={styles.footer__credit}></div>
+      </main>
+      <footer className={`${styles.footer} ${styles.noSelect}`}>
+        <div>
+          <p className={styles.footer__ranking}>Ranking</p>
+          <p className={styles.footer__history}>Round History</p>
+        </div>
+        <div className={styles.footer__player}>
+          <div className={styles.footer__playerChip}>
+            <p>$5</p>
+          </div>
+          <div className={styles.footer__playerChip}>
+            <p>$10</p>
+          </div>
+          <div className={styles.footer__playerChip}>
+            <p>$25</p>
+          </div>
+          <div className={styles.footer__playerChip}>
+            <p>$100</p>
+          </div>
+        </div>
+        <p>Credit: $1000</p>
       </footer>
     </div>
   );
