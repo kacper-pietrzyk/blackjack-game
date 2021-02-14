89c3a625e2ae30e9dbe9ae76787116aa7bcaf68d
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 11:49:37 +0100

    create new component BackgroundText

---
+++ src/components/BackgroundText/BackgroundText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './BackgroundText.module.scss';
+
+const BackgroundText = () => {
+  return (
+    <div className={`${styles.backgroundTextWrapper} ${styles.noSelect}`}>
+      <h1 className={styles.backgroundTextWrapper__text}>Blackjack pays 3 to 2</h1>
+      <h2 className={styles.backgroundTextWrapper__text}>Dealer must stand on 17 and must draw to 16</h2>
+    </div>
+  );
+}
+
+export default BackgroundText;
