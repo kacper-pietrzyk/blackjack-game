f7007f6c017c3a52c611543db66ea17cfa048520
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 14:09:17 +0100

    move Settings to Header folder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './Header.module.scss';
 
-import Settings from '../Settings/Settings';
+import Settings from './Settings/Settings';
 
 const Header = () => {
   return (
