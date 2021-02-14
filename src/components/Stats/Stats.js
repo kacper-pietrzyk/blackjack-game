76d9d781e2151a9eb468b78010c67c299d0fc184
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 11:50:50 +0100

    create new component Stats

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Ranking from './Ranking/Ranking';
+import RoundHistory from './RoundHistory/RoundHistory';
+
+const Stats = () => {
+  return (
+    <div>
+      <Ranking />
+      <RoundHistory />
+    </div>
+  );
+}
+
+export default Stats;
