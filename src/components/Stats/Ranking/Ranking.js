c18430c757fd44cc8fec92afd2ef410ad57f92e5
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 11:51:03 +0100

    create new component Ranking

---
+++ src/components/Stats/Ranking/Ranking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Ranking.module.scss';
+
+const Ranking = () => {
+  return (
+    <p className={styles.ranking}>Ranking</p>
+  );
+}
+
+export default Ranking;
