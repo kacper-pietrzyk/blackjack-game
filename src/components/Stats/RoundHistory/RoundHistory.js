621e206576cae42853ed934bc3e8627e6485286f
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 11:51:18 +0100

    create new component RoundHistory

---
+++ src/components/Stats/RoundHistory/RoundHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './RoundHistory.module.scss';
+
+const RoundHistory = () => {
+  return (
+    <p className={styles.roundHistory}>Round History</p>
+  );
+}
+
+export default RoundHistory;
