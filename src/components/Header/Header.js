2afd20a9988aa8fd6288ec779f3a9e3893e82b67
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 17:37:59 +0100

    useContext implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import styles from './Header.module.scss';
 
 import Settings from './Settings/Settings';
 
+import { AppContext } from '../AppContext/AppContext';
+
 const Header = () => {
+
+  const { roundNumber } = useContext(AppContext);
+
   return (
     <header className={styles.header}>
       <Settings />
-      <p className={`${styles.header__round} ${styles.noSelect}`}>Round 2</p>
+      <p className={`${styles.header__round} ${styles.noSelect}`}>Round {roundNumber}</p>
     </header>
   );
 }
