7da9761c98d23a4655c73948f8462c8f8e4a052d
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-14 11:52:41 +0100

    split Root into smaller components

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,45 +1,26 @@
 import React from 'react';
 import styles from './Root.module.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSave, faFolderOpen, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import Header from '../Header/Header';
+import Stats from '../Stats/Stats';
+import Tokens from '../Tokens/Tokens';
+import Actions from '../Actions/Actions';
+import BackgroundText from '../BackgroundText/BackgroundText';
 
 const Root = () => {
   return (
     <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <div className={styles.header__optionsWrapper}>
-          <FontAwesomeIcon icon={faSave} className={styles.header__option} />
-          <FontAwesomeIcon icon={faFolderOpen} className={styles.header__option} />
-          <FontAwesomeIcon icon={faSyncAlt} className={styles.header__option} />
-        </div>
-        <p className={`${styles.header__round} ${styles.noSelect}`}>Round 2</p>
-      </header>
+      <Header />
       <main className={styles.main}>
-        <div className={`${styles.main__backgroundTextWrapper} ${styles.noSelect}`}>
-          <h1 className={styles.main__backgroundTextWrapper_text}>Blackjack pays 3 to 2</h1>
-          <h2 className={styles.main__backgroundTextWrapper_text}>Dealer must stand on 17 and must draw to 16</h2>
-        </div>
+        <BackgroundText />
+        <div className={styles.main__dealerCards}></div>
+        <div className={styles.main__playerCards}></div>
+        <div className={styles.main__bet}></div>
       </main>
       <footer className={`${styles.footer} ${styles.noSelect}`}>
-        <div>
-          <p className={styles.footer__ranking}>Ranking</p>
-          <p className={styles.footer__history}>Round History</p>
-        </div>
-        <div className={styles.footer__player}>
-          <div className={styles.footer__playerChip}>
-            <p>$5</p>
-          </div>
-          <div className={styles.footer__playerChip}>
-            <p>$10</p>
-          </div>
-          <div className={styles.footer__playerChip}>
-            <p>$25</p>
-          </div>
-          <div className={styles.footer__playerChip}>
-            <p>$100</p>
-          </div>
-        </div>
+        <Stats />
+        {/* <Tokens /> */}
+        <Actions />
         <p>Credit: $1000</p>
       </footer>
     </div>
