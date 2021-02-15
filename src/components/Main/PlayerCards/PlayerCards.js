af95d9adcc018a6b7c9558c1d7e8089d1bdca828
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-15 21:57:44 +0100

    create new component PlayerCards

--- src/components/Main/PlayerCards/PlayerCards.js
+++ src/components/Main/PlayerCards/PlayerCards.js
@@ -1,16 +1,59 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styles from './PlayerCards.module.scss';
 
-import { AppContext } from '../../AppContext/AppContext';
+import CardPicture from '../CardPicture/CardPicture';
 
-const PlayerCards = () => {
+// import { AppContext } from '../../AppContext/AppContext';
 
-  const { playerSum } = useContext(AppContext);
+const PlayerCards = ({ cards }) => {
+
+  // const { dealerCards, userCards } = useContext(AppContext);
+
+  const getPlayerCardsSum = () => {
+
+    const allValues = cards.map(card => {
+      if (card.value === "JACK" ||
+        card.value === "QUEEN" ||
+        card.value === "KING") {
+        return 10;
+      } else if (card.value === "ACE") {
+        return card.value;
+      }
+      else {
+        return Number(card.value);
+      }
+    })
+
+    const numberValues = allValues.filter(value => Number.isInteger(value));
+
+    const aceValues = allValues.filter(value => value === "ACE");
+
+    const integerSum = numberValues.reduce((prevValue, value) => {
+      return prevValue + value;
+    })
+    let sum = integerSum;
+    if (aceValues.length) {
+      aceValues.forEach(() => {
+        if (sum <= 10) {
+          sum += 11;
+        } else {
+          sum += 1;
+        }
+      })
+    }
+
+    return sum;
+  }
+  const playerCardsSum = getPlayerCardsSum();
+
+  const cardsImg = cards.map(card => (
+    <CardPicture key={card.code} src={card.images.png} />
+  ))
 
   return (
     <div className={styles.player}>
-      <div className={styles.player__cards}></div>
-      {playerSum > 0 && <p className={styles.player__cardsValue}>{playerSum}</p>}
+      {cardsImg}
+      {playerCardsSum > 0 && <p className={styles.player__cardsValue}>{playerCardsSum}</p>}
     </div>
   );
 }
