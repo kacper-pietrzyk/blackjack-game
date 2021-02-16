ab50aa433b7409ccaeabb6a628f3a6f1cc450e5e
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-17 00:22:16 +0100

    create new component ResultMessage

---
+++ src/components/Main/ResultMessage/ResultMessage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './ResultMessage.module.scss';
+
+const ResultMessage = ({ winner }) => {
+
+  const userIsWinner = (
+    <>
+      <h2 className={styles.result__header}>Dealer bust!</h2>
+      <h3 className={styles.result__header}>You win</h3>
+    </>
+  )
+
+  const dealerIsWinner = (
+    <>
+      <h2 className={`${styles.result__header} ${styles.result__header_big}`}>Bust!</h2>
+      <h3 className={styles.result__header}>You lost</h3>
+    </>
+  )
+
+  const draw = (
+    <>
+      <h2 className={`${styles.result__header} ${styles.result__header_big}`}>Draw!</h2>
+    </>
+  )
+
+  return (
+    <div className={styles.resultWrapper}>
+      <div className={styles.result}>
+        {winner === "user" && userIsWinner}
+        {winner === "dealer" && dealerIsWinner}
+        {winner === "draw" && draw}
+      </div>
+      <button className={styles.result__nextRound}>Next round</button>
+    </div>
+  );
+}
+
+export default ResultMessage;
