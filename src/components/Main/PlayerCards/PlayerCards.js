5c8280bdd170e59a6cbecbedb8723faa8d3031fe
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-17 00:21:59 +0100

    move playerCardsSum calculation to other component

--- src/components/Main/PlayerCards/PlayerCards.js
+++ src/components/Main/PlayerCards/PlayerCards.js
@@ -3,51 +3,10 @@ import styles from './PlayerCards.module.scss';
 
 import CardPicture from '../CardPicture/CardPicture';
 
-// import { AppContext } from '../../AppContext/AppContext';
-
-const PlayerCards = ({ cards }) => {
-
-  // const { dealerCards, userCards } = useContext(AppContext);
-
-  const getPlayerCardsSum = () => {
-
-    const allValues = cards.map(card => {
-      if (card.value === "JACK" ||
-        card.value === "QUEEN" ||
-        card.value === "KING") {
-        return 10;
-      } else if (card.value === "ACE") {
-        return card.value;
-      }
-      else {
-        return Number(card.value);
-      }
-    })
-
-    const numberValues = allValues.filter(value => Number.isInteger(value));
-
-    const aceValues = allValues.filter(value => value === "ACE");
-
-    const integerSum = numberValues.reduce((prevValue, value) => {
-      return prevValue + value;
-    })
-    let sum = integerSum;
-    if (aceValues.length) {
-      aceValues.forEach(() => {
-        if (sum <= 10) {
-          sum += 11;
-        } else {
-          sum += 1;
-        }
-      })
-    }
-
-    return sum;
-  }
-  const playerCardsSum = getPlayerCardsSum();
+const PlayerCards = ({ cards, playerCardsSum }) => {
 
   const cardsImg = cards.map(card => (
-    <CardPicture key={card.code} src={card.images.png} />
+    <CardPicture key={card.code + Math.floor(Math.random() * 10000)} src={card.images.png} />
   ))
 
   return (
