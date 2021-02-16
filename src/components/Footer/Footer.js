bc586373fcab2e577314efc3d4012f1ffe234b0f
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-17 00:17:00 +0100

    correction of conditional render

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
 import styles from './Footer.module.scss';
 
+import Actions from './Actions/Actions';
 import Stats from './Stats/Stats';
 import Tokens from './Tokens/Tokens';
-import Actions from './Actions/Actions';
 
 import { AppContext } from '../AppContext/AppContext';
 
 const Footer = () => {
 
-  const { credit, isDealAccepted, isUserTurnDone } = useContext(AppContext);
+  const {
+    credit,
+    isDealAccepted } = useContext(AppContext);
 
   return (
     <footer className={`${styles.footer} ${styles.noSelect}`}>
       <Stats />
       {!isDealAccepted && <Tokens />}
-      {(isDealAccepted && !isUserTurnDone) && <Actions />}
+      {isDealAccepted && <Actions />}
       <p>Credit: ${credit}</p>
     </footer>
   );
