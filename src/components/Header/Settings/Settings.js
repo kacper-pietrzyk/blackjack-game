77783dcd25481a2612c58c3bcb38c1083473a326
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-17 04:59:51 +0100

    save, load and reset updates

--- src/components/Header/Settings/Settings.js
+++ src/components/Header/Settings/Settings.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import styles from './Settings.module.scss';
+
+import { AppContext } from '../../AppContext/AppContext';
+import { resetGame } from '../../Main/resetGame';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSave, faFolderOpen, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
-const Settings = () => {
+const Settings = ({ saveGame, loadGame }) => {
+
+  const state = useContext(AppContext);
+  const { saveActive, loadActive } = useContext(AppContext);
+
+  const [resetActive, setResetActive] = useState(false);
+
+  const handleReset = () => {
+
+    resetGame(
+      state.setCredit,
+      state.setBet,
+      state.setDealerCards,
+      state.setDealerCardsSum,
+      state.setDeck,
+      state.setIsDealAccepted,
+      state.setIsDoubleDownAvailable,
+      state.setIsUserTurnFinished,
+      state.setWinner,
+      state.setRoundNumber,
+      state.setUserCards,
+      state.setUserCardsSum
+    )
+
+    setResetActive(true);
+
+    setTimeout(() => {
+      setResetActive(false);
+    }, 1500);
+  }
+
+
+
   return (
     <div>
-      <FontAwesomeIcon icon={faSave} className={styles.option} />
-      <FontAwesomeIcon icon={faFolderOpen} className={styles.option} />
-      <FontAwesomeIcon icon={faSyncAlt} className={styles.option} />
+      <FontAwesomeIcon icon={faSave} className={styles.option} onClick={saveGame} title="Zapisz grę" />
+      <FontAwesomeIcon icon={faFolderOpen} className={styles.option} onClick={loadGame} title="Wczytaj grę" />
+      <FontAwesomeIcon icon={faSyncAlt} className={styles.option} onClick={handleReset} title="Zresetuj grę" />
+      {resetActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Reset...</h2></div>}
+      {saveActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Save...</h2></div>}
+      {loadActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Load...</h2></div>}
     </div>
   );
 }
