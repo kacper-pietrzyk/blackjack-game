969a1370cc6c29fbcf7ed649a08893595f293aeb
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-17 04:58:54 +0100

    delete all-in text

--- src/components/Footer/Tokens/Tokens.js
+++ src/components/Footer/Tokens/Tokens.js
@@ -21,7 +21,7 @@ const Tokens = () => {
       {credit >= 10 && <button className={styles.tokensWrapper__token} value={10} onClick={increaseBet}>$10</button>}
       {credit >= 25 && <button className={styles.tokensWrapper__token} value={25} onClick={increaseBet}>$25</button>}
       {credit >= 100 && <button className={styles.tokensWrapper__token} value={100} onClick={increaseBet}>$100</button>}
-      {credit === 0 && <h2 className={styles.tokensWrapper__allIn}>All-in</h2>}
+      {/* {credit === 0 && <h2 className={styles.tokensWrapper__allIn}>All-in</h2>} */}
     </div>
   );
 }
