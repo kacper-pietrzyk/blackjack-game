cf1469c0fa099bc842ae463ae28d8c60fad402ef
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-19 23:19:53 +0100

    update info for user and names of functions

--- src/components/Main/ResultMessage/ResultMessage.js
+++ src/components/Main/ResultMessage/ResultMessage.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './ResultMessage.module.scss';
 
-const ResultMessage = ({ credit, winner, click, roundNumber }) => {
+const ResultMessage = ({ bet, credit, winner, newGame, roundNumber }) => {
 
   const userIsWinner = (
     <>
-      <h2 className={styles.result__header}>Dealer bust!</h2>
-      <h3 className={styles.result__header}>You win</h3>
+      <h2 className={styles.result__header}>Dealer loses!</h2>
+      <h3 className={styles.result__header}>You win ${1.5 * bet}</h3>
     </>
   )
 
   const dealerIsWinner = (
     <>
-      <h2 className={`${styles.result__header} ${styles.result__header_big}`}>Bust!</h2>
-      <h3 className={styles.result__header}>You lost</h3>
+      <h2 className={`${styles.result__header} ${styles.result__header_big}`}>Dealer wins!</h2>
+      <h3 className={styles.result__header}>You lose ${bet}</h3>
     </>
   )
 
@@ -38,8 +38,8 @@ const ResultMessage = ({ credit, winner, click, roundNumber }) => {
         {winner === "dealer" && dealerIsWinner}
         {winner === "draw" && draw}
       </div>
-      {winner && <button className={styles.result__nextGame} onClick={click}>Next round</button>}
-      {(credit === 0 || (roundNumber === 5)) && <button className={styles.result__nextGame} onClick={click}>New game</button>}
+      {winner && <button className={styles.result__nextGame} onClick={newGame}>Next round</button>}
+      {(credit === 0 || (roundNumber === 5)) && <button className={styles.result__nextGame} onClick={newGame}>New game</button>}
     </div>
   );
 }
