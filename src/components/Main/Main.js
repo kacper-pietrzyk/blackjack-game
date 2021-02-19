1883e419695da432f9c829ae4fb6323620a7d7ad
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-19 23:16:12 +0100

    improve implementation of getting cards

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,26 +8,27 @@ import ResultMessage from './ResultMessage/ResultMessage';
 
 import { AppContext } from '../AppContext/AppContext';
 import { getCards } from '../Root/getCards';
+import { getPlayerCardsSum } from '../Root/getPlayerCardsSum';
 import { resetGame } from './resetGame';
 
 const Main = () => {
 
   const {
     bet, setBet,
+    credit, setCredit,
     deck, setDeck,
     dealerCards, setDealerCards,
+    dealerCardsSum, setDealerCardsSum,
     setIsDealAccepted,
-    winner,
+    setIsDoubleDownAvailable,
+    setIsUserTurnFinished,
     userCards, setUserCards,
     userCardsSum, setUserCardsSum,
-    setIsUserTurnFinished,
-    setIsDoubleDownAvailable,
-    setWinner,
-    setCredit, credit,
     setRoundNumber, roundNumber,
-    dealerCardsSum, setDealerCardsSum } = useContext(AppContext);
+    winner, setWinner } = useContext(AppContext);
 
-  const getNewDeck = () => {
+  const startNewDeal = () => {
+    // get new deck if it's first round
     if (roundNumber === 1) {
       const newShuffledCardsUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6';
       fetch(newShuffledCardsUrl)
@@ -43,15 +44,23 @@ const Main = () => {
         })
         .catch(err => console.log(`${err}: Failed to get cards from API`));
     } else {
+      // if it's not first round - get cards from current deck
       getInitialCards(deck.deck_id)
     }
   };
 
-  const getInitialCards = deckId => {
+  const getInitialCards = async (deckId) => {
     // get user cards
-    getCards(deckId, 2, userCards, setUserCards, setUserCardsSum);
+    const initialUserCards = await getCards(deckId, 2);
+    setUserCards(initialUserCards);
+    const userSum = getPlayerCardsSum(initialUserCards);
+    setUserCardsSum(userSum);
+
     // get dealer card
-    getCards(deckId, 1, dealerCards, setDealerCards, setDealerCardsSum);
+    const initialDealerCards = await getCards(deckId, 1);
+    setDealerCards(initialDealerCards);
+    const dealerSum = getPlayerCardsSum(initialDealerCards);
+    setDealerCardsSum(dealerSum);
   }
 
   const handleNextRound = () => {
@@ -61,15 +70,15 @@ const Main = () => {
       setCredit(credit + bet);
     }
     setBet(0);
-    setDealerCards(null);
+    setDealerCards([]);
     setDealerCardsSum(0);
     setIsDealAccepted(false);
     setIsDoubleDownAvailable(true);
     setIsUserTurnFinished(false);
-    setWinner('');
     setRoundNumber(roundNumber + 1);
-    setUserCards(null);
+    setUserCards([]);
     setUserCardsSum(0);
+    setWinner('');
   }
 
   const handleNewGame = () => {
@@ -83,50 +92,52 @@ const Main = () => {
       setIsDealAccepted,
       setIsDoubleDownAvailable,
       setIsUserTurnFinished,
-      setWinner,
       setRoundNumber,
       setUserCards,
-      setUserCardsSum
+      setUserCardsSum,
+      setWinner
     )
   }
 
   return (
     <main className={styles.main}>
-      {dealerCards &&
+      {dealerCards.length > 0 &&
         <PlayerCards
           cards={dealerCards}
           playerCardsSum={dealerCardsSum}
         />}
       <BackgroundText />
-      {((credit === 0 && winner === "dealer") || (roundNumber === 5 && winner)) &&
-        <ResultMessage
-          credit={credit}
-          click={handleNewGame}
-          roundNumber={roundNumber}
-        />}
-      {(winner === "dealer" && roundNumber < 5 && credit >= 5) &&
-        <ResultMessage
-          winner="dealer"
-          click={handleNextRound}
-        />}
-      {(winner === "user" && roundNumber < 5 && credit >= 5) &&
-        <ResultMessage
-          winner="user"
-          click={handleNextRound}
-        />}
-      {(winner === "draw" && roundNumber < 5 && credit >= 5) &&
-        <ResultMessage
-          winner="draw"
-          click={handleNextRound}
-        />}
-      {userCards &&
+      {userCards.length > 0 &&
         <PlayerCards
           cards={userCards}
           playerCardsSum={userCardsSum}
         />}
       {bet !== 0 &&
         <Bet
-          getNewDeck={getNewDeck}
+          startNewDeal={startNewDeal}
+        />}
+      {((credit === 0 && winner === "dealer") || (roundNumber === 5 && winner)) &&
+        <ResultMessage
+          credit={credit}
+          newGame={handleNewGame}
+          roundNumber={roundNumber}
+        />}
+      {(winner === "dealer" && roundNumber < 5 && credit >= 5) &&
+        <ResultMessage
+          bet={bet}
+          winner="dealer"
+          newGame={handleNextRound}
+        />}
+      {(winner === "user" && roundNumber < 5) &&
+        <ResultMessage
+          bet={bet}
+          winner="user"
+          newGame={handleNextRound}
+        />}
+      {(winner === "draw" && roundNumber < 5) &&
+        <ResultMessage
+          winner="draw"
+          newGame={handleNextRound}
         />}
     </main>
   );
