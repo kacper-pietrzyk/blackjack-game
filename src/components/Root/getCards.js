b57412258895ddddd1fd8e5dda567ae7db069297
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-19 23:20:30 +0100

    improve implementation of function getCards

--- src/components/Root/getCards.js
+++ src/components/Root/getCards.js
@@ -1,25 +1,23 @@
-import { getPlayerCardsSum } from './getPlayerCardsSum';
+export const getCards = (deckId, numberOfCards) => {
 
-export const getCards = (deckId, numberOfCards, playerCards, setPlayerCards, setPlayerSum) => {
   const cardsUrl = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numberOfCards}`;
 
-  fetch(cardsUrl)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json()
-      }
-      throw Error(response.statusText);
-    })
-    .then(result => {
-      if (playerCards) {
-        setPlayerCards([...playerCards, ...result.cards]);
-        const playerSum = getPlayerCardsSum([...playerCards, ...result.cards]);
-        setPlayerSum(playerSum);
-      } else {
-        setPlayerCards(result.cards);
-        const playerSum = getPlayerCardsSum(result.cards);
-        setPlayerSum(playerSum);
-      }
-    })
-    .catch(err => console.log(`${err}: Failed to get cards from API`));
+  return new Promise((resolve, reject) => {
+
+    fetch(cardsUrl)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json()
+        }
+        throw Error(response.statusText);
+      })
+      .then(result => {
+        resolve(result.cards)
+        reject('Failed to get cards')
+      })
+      .catch(err => {
+        console.log(`${err}: Failed to get cards from API`);
+        alert("Server error - unfortunately the cards could not be loaded. Please try start new game.");
+      });
+  })
 }
