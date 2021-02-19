8f2de9ce6ad70a0e9a450e137882db2879f9f98e
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-19 23:13:12 +0100

    add all-in token

--- src/components/Footer/Tokens/Tokens.js
+++ src/components/Footer/Tokens/Tokens.js
@@ -17,10 +17,11 @@ const Tokens = () => {
 
   return (
     <div className={styles.tokensWrapper}>
-      {credit >= 5 && <button className={styles.tokensWrapper__token} value={5} onClick={increaseBet}>$5</button>}
-      {credit >= 10 && <button className={styles.tokensWrapper__token} value={10} onClick={increaseBet}>$10</button>}
-      {credit >= 25 && <button className={styles.tokensWrapper__token} value={25} onClick={increaseBet}>$25</button>}
-      {credit >= 100 && <button className={styles.tokensWrapper__token} value={100} onClick={increaseBet}>$100</button>}
+      {credit >= 5 && <button className={`${styles.tokensWrapper__token} ${styles.tokensWrapper__token_5}`} value={5} onClick={increaseBet}>$5</button>}
+      {credit >= 10 && <button className={`${styles.tokensWrapper__token} ${styles.tokensWrapper__token_10}`} value={10} onClick={increaseBet}>$10</button>}
+      {credit >= 25 && <button className={`${styles.tokensWrapper__token} ${styles.tokensWrapper__token_25}`} value={25} onClick={increaseBet}>$25</button>}
+      {credit >= 100 && <button className={`${styles.tokensWrapper__token} ${styles.tokensWrapper__token_100}`} value={100} onClick={increaseBet}>$100</button>}
+      {credit > 0 && <button className={`${styles.tokensWrapper__token} ${styles.tokensWrapper__token_allIn}`} value={credit} onClick={increaseBet}>All-in</button>}
       {/* {credit === 0 && <h2 className={styles.tokensWrapper__allIn}>All-in</h2>} */}
     </div>
   );
