0e5aa4b63467bb625956f243dc43fd5c9af1fc4d
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-20 23:29:38 +0100

    implement handleRoundHistory function

--- src/components/Footer/Stats/Stats.js
+++ src/components/Footer/Stats/Stats.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styles from './Stats.module.scss';
 
 import Ranking from './Ranking/Ranking';
 import RoundHistory from './RoundHistory/RoundHistory';
 
 const Stats = () => {
+
+  const [showRoundHistory, setShowRoundHistory] = useState(false);
+
+  let showRanking = false;
+
+  const handleRoundHistory = e => {
+    if (e.target.name === "show") {
+      setShowRoundHistory(true);
+    } else {
+      setShowRoundHistory(false);
+    }
+  }
+
   return (
-    <div>
-      <Ranking />
-      <RoundHistory />
-    </div>
+    <div className={styles.stats}>
+      <button className={styles.stats__button}>Ranking</button>
+      {showRanking && <Ranking />}
+      <button className={styles.stats__button} name="show" onClick={handleRoundHistory}>Round History</button>
+      {showRoundHistory && <div className={styles.gamePause}></div>}
+      {showRoundHistory && <RoundHistory handleRoundHistory={handleRoundHistory} />}
+    </div >
   );
 }
 
