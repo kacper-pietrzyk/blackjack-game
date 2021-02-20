fa1e704d82b6a43186dfc702e1113c85ee178f47
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-20 19:54:27 +0100

    add roundHistory to reset

--- src/components/Main/resetGame.js
+++ src/components/Main/resetGame.js
@@ -1,7 +1,19 @@
-export const resetGame = (setCredit, setBet, setDealerCards, setDealerCardsSum, setDeck, setIsDealAccepted, setIsDoubleDownAvailable, setIsUserTurnFinished, setRoundNumber, setUserCards, setUserCardsSum, setWinner) => {
+export const resetGame = (setBet, setCredit, setDealerCards, setDealerCardsSum, setDeck, setIsDealAccepted, setIsDoubleDownAvailable, setIsUserTurnFinished, setRoundNumber, setHistory, setUserCards, setUserCardsSum, setWinner) => {
 
-  setCredit(1000)
+  const roundHistory = {
+    bet: ["Bet"],
+    credit: ["Credit"],
+    dealerCards: ["Dealer cards"],
+    dealerSum: ["Dealer sum"],
+    userCards: ["Your cards"],
+    userSum: ["Your sum"],
+    profit: ["Profit/Loss"],
+    roundNumber: ["Round"],
+    winner: ["Winner"],
+  }
+
   setBet(0);
+  setCredit(1000);
   setDealerCards([]);
   setDealerCardsSum(0);
   setDeck()
@@ -9,6 +21,7 @@ export const resetGame = (setCredit, setBet, setDealerCards, setDealerCardsSum, 
   setIsDoubleDownAvailable(true);
   setIsUserTurnFinished(false);
   setRoundNumber(1);
+  setHistory(roundHistory);
   setUserCards([]);
   setUserCardsSum(0);
   setWinner('');
