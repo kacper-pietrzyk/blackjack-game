fac575fd0b4257638c63900ba8e6bd8ccf7e1fb7
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-21 17:12:18 +0100

    implement function handleShowInfo

--- src/components/Header/Settings/Settings.js
+++ src/components/Header/Settings/Settings.js
@@ -1,17 +1,20 @@
 import React, { useState, useContext } from 'react';
 import styles from './Settings.module.scss';
 
+import Info from './Info/Info';
+
 import { AppContext } from '../../AppContext/AppContext';
 import { resetGame } from '../../Main/resetGame';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSave, faFolderOpen, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSave, faFolderOpen, faSyncAlt, faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 const Settings = ({ saveGame, loadGame }) => {
 
   const state = useContext(AppContext);
 
   const [isResetActive, setIsResetActive] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
   const handleReset = () => {
 
@@ -38,14 +41,62 @@ const Settings = ({ saveGame, loadGame }) => {
     }, 1000);
   }
 
+  const handleShowInfo = e => {
+    if (e.target.name === "close") {
+      setShowInfo(false);
+    } else {
+      setShowInfo(true);
+    }
+  }
+
   return (
     <div>
-      <FontAwesomeIcon icon={faSave} className={styles.option} onClick={saveGame} title="Save game" />
-      <FontAwesomeIcon icon={faFolderOpen} className={styles.option} onClick={loadGame} title="Load game" />
-      <FontAwesomeIcon icon={faSyncAlt} className={styles.option} onClick={handleReset} title="Reset" />
-      {state.isSaveActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Saving...</h2></div>}
-      {state.isLoadActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Loading...</h2></div>}
-      {isResetActive && <div className={styles.resetMessage}><h2 className={styles.resetMessage__text}>Reset...</h2></div>}
+      <FontAwesomeIcon
+        icon={faSave}
+        className={styles.option}
+        onClick={saveGame}
+        title="Save game"
+      />
+      <FontAwesomeIcon
+        icon={faFolderOpen}
+        className={styles.option}
+        onClick={loadGame}
+        title="Load game"
+      />
+      <FontAwesomeIcon
+        icon={faSyncAlt}
+        className={styles.option}
+        onClick={handleReset}
+        title="Reset"
+      />
+      <FontAwesomeIcon
+        icon={faQuestion}
+        className={styles.option}
+        onClick={handleShowInfo}
+        title="Info"
+      />
+      {showInfo &&
+        <div
+          className={styles.gamePause}>
+        </div>}
+      {showInfo &&
+        <Info
+          handleShowInfo={handleShowInfo} />}
+      {state.isSaveActive &&
+        <div
+          className={styles.resetMessage}>
+          <h2 className={styles.resetMessage__text}>Saving...</h2>
+        </div>}
+      {state.isLoadActive &&
+        <div
+          className={styles.resetMessage}>
+          <h2 className={styles.resetMessage__text}>Loading...</h2>
+        </div>}
+      {isResetActive &&
+        <div
+          className={styles.resetMessage}>
+          <h2 className={styles.resetMessage__text}>Reset...</h2>
+        </div>}
     </div>
   );
 }
