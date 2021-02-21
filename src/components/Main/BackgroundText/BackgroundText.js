e170b0e8620ed772e1e7bd2e8668b26c4f837bd1
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-21 17:14:05 +0100

    add text 'please place a bet'

--- src/components/Main/BackgroundText/BackgroundText.js
+++ src/components/Main/BackgroundText/BackgroundText.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './BackgroundText.module.scss';
 
-const BackgroundText = () => {
+const BackgroundText = ({ bet }) => {
   return (
     <div className={`${styles.backgroundTextWrapper} ${styles.noSelect}`}>
       <h1 className={styles.backgroundTextWrapper__text}>Blackjack pays 3 to 2</h1>
       <h2 className={styles.backgroundTextWrapper__text}>Dealer must stand on 17 and must draw to 16</h2>
+      {!bet && <h2 className={styles.backgroundTextWrapper__betInfo}>Please place a bet</h2>}
     </div>
   );
 }
