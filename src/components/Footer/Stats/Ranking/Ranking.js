7a85fd781d7d9eb6a4fc2c31a887b546d150936b
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-21 17:11:03 +0100

    rename function

--- src/components/Footer/Stats/Ranking/Ranking.js
+++ src/components/Footer/Stats/Ranking/Ranking.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Ranking.module.scss';
 
-const Ranking = ({ handleRanking }) => {
+const Ranking = ({ handleShowRanking }) => {
 
   let rankingToLoad = JSON.parse(localStorage.getItem("ranking"));
   //rankingToLoad === null in first game - when there isn't currentRanking in localStorage
@@ -21,9 +21,9 @@ const Ranking = ({ handleRanking }) => {
   ));
 
   return (
-    <div className={styles.ranking} >
+    <div className={styles.ranking}>
       <h2 className={styles.ranking__header}>Top 5 historic results</h2>
-      <button className={styles.ranking__close} onClick={handleRanking}>&times;</button>
+      <button className={styles.ranking__close} onClick={handleShowRanking}>&times;</button>
       <ul className={styles.ranking__list}>
         <li className={styles.ranking__listItem}>
           <p>No.</p>
